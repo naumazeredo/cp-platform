5ca8d8ac1f55fba058d41a28a043af850c6645bd
Naum Azeredo <naumazeredo@gmail.com>
2016-07-08 15:02:48 -0300

    Changed page title

--- routes/articles.js
+++ routes/articles.js
@@ -2,18 +2,19 @@ var express = require('express');
 var router = express.Router();
 var markdown = require('marked');
 
-/* GET articles list page. */
+/* GET articles listing. */
 router.get('/', function(req, res, next) {
-  res.render('articles', { title: 'CP Platform - Articles' });
+  res.render('articles', { title: 'Articles' });
 });
 
-/* GET article page */
+/* GET article. */
 router.get('/:id', function(req, res, next) {
   var categories = [{title: 'Dynamic Programming', tag: 'dp', url: '#'}, {title: 'Graphs', tag: 'graphs', url: '#'}];
   var author = {name: 'kogyblack', rating: 4};
   var date = new Date(2015, 6, 1);
   var content = markdown('# Article Title\n\n## This is the content\n\nText here just to complete\n\n- A\n- Simple\n- List\n\nJust to test things out\n');
-  res.render('article', { title: 'CP Platform - Article', categories: categories, author: author, date: date, content: content });
+  var title = 'Article title';
+  res.render('article', { title: title, categories: categories, author: author, date: date, content: content });
 });
 
 module.exports = router;
