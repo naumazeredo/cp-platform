44fbc7545ed5623f93bf2dd9839109c697185a78
Naum Azeredo <naumazeredo@gmail.com>
2016-07-08 01:14:24 -0300

    Added basic article page and renamed difficulty to rating

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var articles = require('./routes/articles');
 var users = require('./routes/users');
 
 var app = express();
+app.locals.moment = require('moment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var markdown = require('marked');
 
 /* GET articles list page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,11 @@ router.get('/', function(req, res, next) {
 
 /* GET article page */
 router.get('/:id', function(req, res, next) {
-  res.render('article', { title: 'CP Platform - Article' });
+  var categories = [{title: 'Dynamic Programming', tag: 'dp', url: '#'}, {title: 'Graphs', tag: 'graphs', url: '#'}];
+  var author = {name: 'kogyblack', rating: 4};
+  var date = new Date(2015, 6, 1);
+  var content = markdown('# Article Title\n\n## This is the content\n\nText here just to complete\n\n- A\n- Simple\n- List\n\nJust to test things out\n');
+  res.render('article', { title: 'CP Platform - Article', categories: categories, author: author, date: date, content: content });
 });
 
 module.exports = router;
