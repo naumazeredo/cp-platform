ce01a4e548dda0561c7395a6a7a61303bf2888fe
Naum Azeredo <naumazeredo@gmail.com>
2016-07-14 23:34:53 -0300

    Configured Docker Compose

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
-var db = require('monk')(process.env.DB_PATH);
+var util = require('util');
+var mongourl = util.format('mongo:%s/cpplatform', process.env.MONGO_PORT);
+var db = require('monk')(mongourl);
 
 module.exports = db;
